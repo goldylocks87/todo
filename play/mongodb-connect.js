2958859cab2fce626328ddca24fe099532596eae
Rapunzel <ahhlick@gmail.com>
2018-09-01 19:47:46 -0700

    sum mo ish

--- play/mongodb-connect.js
+++ play/mongodb-connect.js
@@ -1,11 +1,4 @@
-// const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
-
-let obj = new ObjectID();
-console.log(obj);
-
-// let user = {name: 'al', age: 30};
-// let {name} = user; // object destructuring
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', 
     (err, db) => {
@@ -13,25 +6,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',
         if(err) return console.log('Couldn\'t connect to db...');
         else console.log('Connected to database...');
 
-        // db.collection('Todos').insertOne({
+        db.collection('Todos').find().toArray()
+        .then( (docs) => {
 
-        // }, 
-        // (err, result) => {
+        }, (err) => {
+            console.log('Connected to database...');
 
-        //     if(err) return console.log('Couldn\'t insert record...');
-        //     else console.log( JSON.stringify(result.ops, undefined, 2) );
-        // });
+        });
 
-        // db.collection('Users').insertOne({
-        //     name: 'Al',
-        //     age: 30,
-        //     location: 'San Mateo, CA'
-        // }, 
-        // (err, result) => {
-
-        //     if(err) return console.log('Couldn\'t insert user doc...');
-        //     else console.log( JSON.stringify(result.ops, undefined, 2) );
-        // });
-
-        db.close();
+        db.close(); // closes connection to server
     });
